9494fe6124de9c2b329469bf122c3540cf957cb9
Tomasz Sterna <tomasz@sterna.link>
2025-04-18 12:29:25 +0200

    Debugger spawn/kill

--- src/activateDebug.ts
+++ src/activateDebug.ts
@@ -2,7 +2,7 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 /*
- * activateDebug.ts containes the shared extension code that can be executed both in node.js and the browser.
+ * activateDebug.ts contains the shared extension code that can be executed both in node.js and the browser.
  */
 
 import * as vscode from "vscode";
@@ -13,7 +13,6 @@ import type {
 	WorkspaceFolder,
 } from "vscode";
 import { Cc65DebugSession } from "./cc65Debug";
-import type { FileAccessor } from "./mockRuntime";
 
 export function activateDebug(
 	context: vscode.ExtensionContext,
@@ -59,32 +58,16 @@ export function activateDebug(
 				}
 			},
 		),
-		vscode.commands.registerCommand(
-			"extension.cc65-dbg.toggleFormatting",
-			(variable) => {
-				const ds = vscode.debug.activeDebugSession;
-				if (ds) {
-					ds.customRequest("toggleFormatting");
-				}
-			},
-		),
-	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand(
-			"extension.cc65-dbg.getProgramName",
-			(config) => {
-				return vscode.window.showInputBox({
-					placeHolder:
-						"Please enter the name of a markdown file in the workspace folder",
-					value: "readme.md",
-				});
-			},
-		),
+		vscode.commands.registerCommand("extension.cc65-dbg.toggleFormatting", (variable) => {
+			const ds = vscode.debug.activeDebugSession;
+			if (ds) {
+				ds.customRequest("toggleFormatting");
+			}
+		}),
 	);
 
 	// register a configuration provider for 'cc65-dbg' debug type
-	const provider = new MockConfigurationProvider();
+	const provider = new Cc65ConfigurationProvider();
 	context.subscriptions.push(
 		vscode.debug.registerDebugConfigurationProvider("cc65-dbg", provider),
 	);
@@ -132,86 +115,9 @@ export function activateDebug(
 	if ("dispose" in factory && typeof factory.dispose === "function") {
 		context.subscriptions.push(factory as vscode.Disposable);
 	}
-
-	// override VS Code's default implementation of the debug hover
-	// here we match only Mock "variables", that are words starting with an '$'
-	context.subscriptions.push(
-		vscode.languages.registerEvaluatableExpressionProvider("markdown", {
-			provideEvaluatableExpression(
-				document: vscode.TextDocument,
-				position: vscode.Position,
-			): vscode.ProviderResult<vscode.EvaluatableExpression> {
-				const VARIABLE_REGEXP = /\$[a-z][a-z0-9]*/gi;
-				const line = document.lineAt(position.line).text;
-
-				let m: RegExpExecArray | null;
-				while ((m = VARIABLE_REGEXP.exec(line))) {
-					const varRange = new vscode.Range(
-						position.line,
-						m.index,
-						position.line,
-						m.index + m[0].length,
-					);
-
-					if (varRange.contains(position)) {
-						return new vscode.EvaluatableExpression(varRange);
-					}
-				}
-				return undefined;
-			},
-		}),
-	);
-
-	// override VS Code's default implementation of the "inline values" feature"
-	context.subscriptions.push(
-		vscode.languages.registerInlineValuesProvider("markdown", {
-			provideInlineValues(
-				document: vscode.TextDocument,
-				viewport: vscode.Range,
-				context: vscode.InlineValueContext,
-			): vscode.ProviderResult<vscode.InlineValue[]> {
-				const allValues: vscode.InlineValue[] = [];
-
-				for (
-					let l = viewport.start.line;
-					l <= context.stoppedLocation.end.line;
-					l++
-				) {
-					const line = document.lineAt(l);
-					const regExp = /\$([a-z][a-z0-9]*)/gi; // variables are words starting with '$'
-					let m: RegExpExecArray | null;
-					do {
-						m = regExp.exec(line.text);
-						if (m) {
-							const varName = m[1];
-							const varRange = new vscode.Range(
-								l,
-								m.index,
-								l,
-								m.index + varName.length,
-							);
-
-							// some literal text
-							//allValues.push(new vscode.InlineValueText(varRange, `${varName}: ${viewport.start.line}`));
-
-							// value found via variable lookup
-							allValues.push(
-								new vscode.InlineValueVariableLookup(varRange, varName, false),
-							);
-
-							// value determined via expression evaluation
-							//allValues.push(new vscode.InlineValueEvaluatableExpression(varRange, varName));
-						}
-					} while (m);
-				}
-
-				return allValues;
-			},
-		}),
-	);
 }
 
-class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
+class Cc65ConfigurationProvider implements vscode.DebugConfigurationProvider {
 	/**
 	 * Massage a debug configuration just before a debug session is being launched,
 	 * e.g. add all missing attributes to the debug configuration.
@@ -224,7 +130,7 @@ class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
 		// if launch.json is missing or empty
 		if (!config.type && !config.request && !config.name) {
 			const editor = vscode.window.activeTextEditor;
-			if (editor && editor.document.languageId === "markdown") {
+			if (editor && ["ca65", "cc65"].includes(editor.document.languageId)) {
 				config.type = "cc65-dbg";
 				config.name = "Launch";
 				config.request = "launch";
@@ -245,23 +151,6 @@ class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
 	}
 }
 
-export const workspaceFileAccessor: FileAccessor = {
-	isWindows: typeof process !== "undefined" && process.platform === "win32",
-	async readFile(path: string): Promise<Uint8Array> {
-		let uri: vscode.Uri;
-		try {
-			uri = pathToUri(path);
-		} catch (e) {
-			return new TextEncoder().encode(`cannot read '${path}'`);
-		}
-
-		return await vscode.workspace.fs.readFile(uri);
-	},
-	async writeFile(path: string, contents: Uint8Array) {
-		await vscode.workspace.fs.writeFile(pathToUri(path), contents);
-	},
-};
-
 function pathToUri(path: string) {
 	try {
 		return vscode.Uri.file(path);
@@ -270,14 +159,10 @@ function pathToUri(path: string) {
 	}
 }
 
-class InlineDebugAdapterFactory
-	implements vscode.DebugAdapterDescriptorFactory
-{
+class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
 	createDebugAdapterDescriptor(
 		_session: vscode.DebugSession,
 	): ProviderResult<vscode.DebugAdapterDescriptor> {
-		return new vscode.DebugAdapterInlineImplementation(
-			new Cc65DebugSession(workspaceFileAccessor),
-		);
+		return new vscode.DebugAdapterInlineImplementation(new Cc65DebugSession());
 	}
 }
